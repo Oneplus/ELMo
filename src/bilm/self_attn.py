109c2aaa9aa006bff40fa39f9d170fc6d43a850d
Oneplus <oneplus.lau@gmail.com>
2018-12-11 22:38:23 +0800

    update

--- src/bilm/self_attn.py
+++ src/bilm/self_attn.py
@@ -78,9 +78,8 @@ class MultiHeadedAttention(torch.nn.Module):
     nbatches = query.size(0)
 
     # 1) Do all the linear projections in batch from d_model => h x d_k
-    query, key, value = \
-      [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
-       for l, x in zip(self.linears, (query, key, value))]
+    query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+                         for l, x in zip(self.linears, (query, key, value))]
 
     # 2) Apply attention on all the projected vectors in batch.
     x, self.attn = attention(query, key, value, mask=mask,
@@ -98,70 +97,101 @@ class SelfAttentiveLBLBiLM(torch.nn.Module):
     self.use_cuda = use_cuda
     self.use_position = config['encoder'].get('position', False)
     self.use_relative_position_weights = config['encoder'].get('relative_position_weights', False)
-    self.num_layers = config['encoder']['n_layers']
+    self.n_layers = n_layers = config['encoder']['n_layers']
+    self.n_highway = n_highway = config['encoder']['n_highway']
+    self.n_heads = n_heads = config['encoder']['n_heads']
+    self.input_size = input_size = config['encoder']['projection_dim']
+    self.width = width = config['encoder']['width']
+    self.hidden_size = hidden_size = config['encoder']['projection_dim']
 
-    self.left_attn = MultiHeadedAttention(config['encoder']['n_heads'],
-                                          config['encoder']['projection_dim'], config['dropout'])
-    self.right_attn = MultiHeadedAttention(config['encoder']['n_heads'],
-                                           config['encoder']['projection_dim'], config['dropout'])
     self.dropout = torch.nn.Dropout(self.config['dropout'])
     self.activation = torch.nn.ReLU()
 
-    width = config['encoder']['width']
-    input_size = config['encoder']['projection_dim']
-    hidden_size = config['encoder']['projection_dim']
+    forward_attns, backward_attns = [], []
+    forward_paddings, backward_paddings = [], []
+    forward_blocks, backward_blocks = [], []
+    forward_weights, backward_weights = [], []
 
-    left_padding = torch.randn(width, hidden_size) / np.sqrt(hidden_size)
-    right_padding = torch.randn(width, hidden_size) / np.sqrt(hidden_size)
-    self.left_padding = torch.nn.Parameter(left_padding, requires_grad=True)
-    self.right_padding = torch.nn.Parameter(right_padding, requires_grad=True)
+    for _ in range(n_layers):
+      forward_attns.append(MultiHeadedAttention(n_heads, hidden_size, config['dropout']))
+      backward_attns.append(MultiHeadedAttention(n_heads, hidden_size, config['dropout']))
+
+      forward_paddings.append(torch.nn.Parameter(torch.randn(width, hidden_size) / np.sqrt(hidden_size)))
+      backward_paddings.append(torch.nn.Parameter(torch.randn(width, hidden_size) / np.sqrt(hidden_size)))
+
+      forward_blocks.append(Highway(hidden_size, n_highway))
+      backward_blocks.append(Highway(hidden_size, n_highway))
+
+      if self.use_relative_position_weights:
+        forward_weights.append(torch.nn.Parameter(torch.randn(width + 1)))
+        backward_weights.append(torch.nn.Parameter(torch.randn(width + 1)))
+
+    self.forward_attns = torch.nn.ModuleList(forward_attns)
+    self.backward_attns = torch.nn.ModuleList(backward_attns)
+
+    self.forward_paddings = torch.nn.ParameterList(forward_paddings)
+    self.backward_paddings = torch.nn.ParameterList(backward_paddings)
+
+    self.forward_blocks = torch.nn.ModuleList(forward_blocks)
+    self.backward_blocks = torch.nn.ModuleList(backward_blocks)
 
     if self.use_relative_position_weights:
-      left_weights = torch.randn(width + 1)
-      right_weights = torch.randn(width + 1)
-      self.left_weights = torch.nn.Parameter(left_weights, requires_grad=True)
-      self.right_weights = torch.nn.Parameter(right_weights, requires_grad=True)
+      self.forward_weights = torch.nn.ParameterList(forward_weights)
+      self.backward_weights = torch.nn.ParameterList(backward_weights)
 
     if self.use_position:
       self.position = PositionalEncoding(config['encoder']['projection_dim'], self.config['dropout'])
 
-    self.left_block = Highway(hidden_size, num_layers=config['encoder']['n_highway'])
-    self.right_block = Highway(hidden_size, num_layers=config['encoder']['n_highway'])
-
-    self.input_size = input_size
-    self.width = width
-
   def forward(self, inputs):
     batch_size, sequence_len, dim = inputs.size()
-    if self.use_position:
-      inputs = self.position(inputs)
-    new_inputs = torch.cat([self.left_padding.expand(batch_size, -1, -1),
-                            inputs,
-                            self.right_padding.expand(batch_size, -1, -1)], dim=1)
-
-    left_mask = local_mask(new_inputs.size(-2), self.width)
-    right_mask = local_mask(new_inputs.size(-2), self.width, left_to_right=False)
-    if self.use_cuda:
-      left_mask = left_mask.cuda()
-      right_mask = right_mask.cuda()
-
-    new_left_inputs = self.left_attn(new_inputs, new_inputs, new_inputs, left_mask)
-    new_right_inputs = self.right_attn(new_inputs, new_inputs, new_inputs, right_mask)
-
     all_layers_along_steps = []
-    for start in range(sequence_len):
-      end = start + self.width
-      left_out = new_left_inputs[:, end, :]
-      right_out = new_right_inputs[:, end, :]
 
-      if self.use_relative_position_weights:
-        left_out = left_out + new_inputs.narrow(1, start, self.width + 1).transpose(-2, -1).matmul(self.left_weights)
-        right_out = right_out + new_inputs.narrow(1, end, self.width + 1).transpose(-2, -1).matmul(self.right_weights)
+    forward_inputs = inputs
+    backward_inputs = inputs
 
-      left_out = self.left_block(left_out)
-      right_out = self.right_block(right_out)
-      out = torch.cat([left_out, right_out], dim=1)
+    forward_mask = local_mask(sequence_len + self.width * 2, self.width)
+    backward_mask = local_mask(sequence_len + self.width * 2, self.width, left_to_right=False)
+    if self.use_cuda:
+      forward_mask = forward_mask.cuda()
+      backward_mask = backward_mask.cuda()
 
-      all_layers_along_steps.append(out.unsqueeze(0))
+    for i in range(self.n_layers):
+      if self.use_position:
+        forward_inputs = self.position(forward_inputs)
+        backward_inputs = self.position(backward_inputs)
 
-    return torch.stack(all_layers_along_steps, dim=2)
+      forward_inputs = torch.cat([self.forward_paddings[i].expand(batch_size, -1, -1),
+                                  forward_inputs,
+                                  self.backward_paddings[i].expand(batch_size, -1, -1)], dim=1)
+      backward_inputs = torch.cat([self.forward_paddings[i].expand(batch_size, -1, -1),
+                                   backward_inputs,
+                                   self.backward_paddings[i].expand(batch_size, -1, -1)], dim=1)
+
+      forward_inputs = self.forward_attns[i](forward_inputs, forward_inputs,
+                                             forward_inputs, forward_mask)
+      backward_inputs = self.backward_attns[i](backward_inputs, backward_inputs,
+                                               backward_inputs, backward_mask)
+
+      forward_steps, backward_steps = [], []
+      for start in range(sequence_len):
+        end = start + self.width
+        forward_output = forward_inputs[:, end, :]
+        backward_output = backward_inputs[:, end, :]
+
+        if self.use_relative_position_weights:
+          forward_output = forward_output + forward_inputs.narrow(1, start, self.width + 1).\
+            transpose(-2, -1).matmul(self.forward_weights[i])
+          backward_output = backward_output + backward_inputs.narrow(1, end, self.width + 1).\
+            transpose(-2, -1).matmul(self.backward_weights[i])
+
+        forward_output = self.forward_blocks[i](forward_output)
+        backward_output = self.backward_blocks[i](backward_output)
+
+        forward_steps.append(forward_output)
+        backward_steps.append(backward_output)
+
+      forward_inputs = torch.stack(forward_steps, dim=1)
+      backward_inputs = torch.stack(backward_steps, dim=1)
+      all_layers_along_steps.append(torch.cat([forward_inputs, backward_inputs], dim=-1))
+
+    return torch.stack(all_layers_along_steps, dim=0)
